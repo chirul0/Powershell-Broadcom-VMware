fe05c361e6a466d8641383428ff56f9aa98635d8
Santiago Nesti Arène <1591986+chirul0@users.noreply.github.com>
2025-03-10 18:07:23 -0400

    Create HostsByCluster.ps1

---
+++ ESXi/HostsByCluster.ps1
@@ -0,0 +1,12 @@
+$vcenter = Read-Host "Enter vCenter"
+$credential = Get-Credential
+Connect-VIServer $vcenter -Credential $credential #-AllLinked -Force [Remove # if using multiple vCenters in linked mode]
+
+$Clusters = Get-Cluster
+foreach ($Cluster in $Clusters)
+{
+
+Write-Host "## #####" $Cluster "##### ##"
+$Cluster | Get-VMHost | Select-Object @{Label = "Host"; Expression = {$_.Name}} | Sort-Object -Property Host | Format-Table -AutoSize -Wrap
+
+}

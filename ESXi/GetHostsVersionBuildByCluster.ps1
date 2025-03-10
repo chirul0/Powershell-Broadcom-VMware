5203523cad5aec759368b36736e9222ac38b3d9d
Santiago Nesti Arène <1591986+chirul0@users.noreply.github.com>
2025-03-10 18:13:42 -0400

    Create GetHostsVersionBuildByCluster.ps1

---
+++ ESXi/GetHostsVersionBuildByCluster.ps1
@@ -0,0 +1,14 @@
+# Pulls ESXi hosts by cluster with ESXi Version and Build
+
+$vcenter = Read-Host "Enter vCenter"
+$credential = Get-Credential
+Connect-VIServer $vcenter -Credential $credential #-AllLinked -Force [Remove # if working with multiple vCenters in linked mode]
+
+$Clusters = Get-Cluster
+foreach ($Cluster in $Clusters)
+{
+
+Write-Host "## #####" $Cluster "##### ##"
+$Cluster | Get-VMHost | Select-Object @{Label = "Host"; Expression = {$_.Name}} , @{Label = "ESX Version"; Expression = {$_.version}}, @{Label = "ESX Build" ; Expression = {$_.build}} | Sort-Object -Property Host | Format-Table -AutoSize -Wrap
+
+}

8d527a8d5cb7c3fea1ad0142b66669dfc4219a82
Santiago Nesti Arène <1591986+chirul0@users.noreply.github.com>
2025-03-24 18:41:08 -0400

    Add SAN RAID FW
    And additional formatting changes

--- ESXi/GetESXiHostBIOSESXiFWVersionsByCluster.ps1
+++ ESXi/GetESXiHostBIOSESXiFWVersionsByCluster.ps1
@@ -3,24 +3,24 @@ $credential = Get-Credential
 Connect-VIServer $vcenter -Credential $credential #-AllLinked -Force #[Remove # before -AllLinked if working with multiple vCenters in linked mode]
 
 Get-Cluster -PipelineVariable cluster |
-Foreach-Object -Process {
-  Write-Host "## #####" $Cluster "##### ##"
+  Foreach-Object -Process {
+    Write-Host "## #####" $Cluster "##### ##"
      ForEach-Object -Process {
       $Cluster | Get-VMHost -PipelineVariable esx |
         ForEach-Object -Process {
           $esxcli = Get-EsxCli -VMHost $esx -V2
-          $esxcli.network.nic.get.invoke(@{nicname="vmnic0"}) | 
-              Select-Object @{N = 'ESXi Host'; E = { $esx.Name }},
+          $esxcli.network.nic.get.invoke( @{ nicname="vmnic0" }) | Select-Object @{ N = 'ESXi Host'; E = { $esx.Name }},
+              #Comment out lines to exclude attributes from the report.
               @{N = 'Vendor'; E = { $esx.ExtensionData.Hardware.SystemInfo.Vendor }},
-              @{N = 'Model'; E = { $esx.ExtensionData.Hardware.SystemInfo.Model}},
-              @{N='Processors';E={$esx.ExtensionData.Hardware.CpuInfo.NumCpuPackages}}, 
+              @{N = 'Model'; E = { $esx.ExtensionData.Hardware.SystemInfo.Model }},
+              @{N = 'Processors';E = { $esx.ExtensionData.Hardware.CpuInfo.NumCpuPackages }}, 
               @{N = 'BIOS Version'; E = { $esx.ExtensionData.Hardware.BiosInfo.BiosVersion }},
               @{N = 'Product'; E = { $esx.ExtensionData.Config.Product.Name }},
               @{N = 'ESXi Version'; E = { $esx.Version }},
               @{N = 'ESXi Build'; E = { $esx.Build }},
-              @{N='NIC Driver';E={$_.DriverINfo.Version}},
-              @{N='NIC FW Version';E={$_.DriverInfo.FirmwareVersion}}
+              @{N = 'NIC Driver'; E = { $_.DriverInfo.Version }},
+              @{N = 'NIC FW Version'; E = { $_.DriverInfo.FirmwareVersion }},
+              @{N = 'RAID FW Version'; E = { $esxcli.storage.san.sas.list.invoke().FirmwareVersion }} 
             }
-        } | Sort-Object -Property "ESXi Build" | #Change this property to change sorting
-        Format-Table -AutoSize -Wrap
-    }  
+      } | Sort-Object -Property "ESXi Host" | Format-Table -AutoSize -Wrap #Change Property value to sort by a different attribute
+    } 

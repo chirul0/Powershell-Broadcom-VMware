d9dfb5cef66b028ab972b96de34695a1672de275
Santiago Nesti Arène <1591986+chirul0@users.noreply.github.com>
2025-03-24 18:43:44 -0400

    Formatting edits

--- ESXi/HostsByCluster.ps1
+++ ESXi/HostsByCluster.ps1
@@ -3,10 +3,12 @@ $credential = Get-Credential
 Connect-VIServer $vcenter -Credential $credential #-AllLinked -Force [Remove # if using multiple vCenters in linked mode]
 
 $Clusters = Get-Cluster
-foreach ($Cluster in $Clusters)
-{
-
-Write-Host "## #####" $Cluster "##### ##"
-$Cluster | Get-VMHost | Select-Object @{Label = "Host"; Expression = {$_.Name}} | Sort-Object -Property Host | Format-Table -AutoSize -Wrap
+  foreach ($Cluster in $Clusters) {
+  
+  Write-Host "## #####" $Cluster "##### ##"
+  $Cluster | Get-VMHost | 
+  Select-Object @{Label = "Host"; Expression = { $_.Name }} | 
+  Sort-Object -Property Host | 
+  Format-Table -AutoSize -Wrap
 
 }

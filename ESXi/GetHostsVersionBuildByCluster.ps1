a5053708944c4451dae3936077b9b866bdc4b791
Santiago Nesti Arène <1591986+chirul0@users.noreply.github.com>
2025-03-24 18:45:52 -0400

    Formatting edits

--- ESXi/GetHostsVersionBuildByCluster.ps1
+++ ESXi/GetHostsVersionBuildByCluster.ps1
@@ -3,13 +3,15 @@
 $vcenter = Read-Host "Enter vCenter"
 $credential = Get-Credential
 Connect-VIServer $vcenter -Credential $credential #-AllLinked -Force #[Remove # before -AllLinked if working with multiple vCenters in linked mode]
+
 $Clusters = Get-Cluster
-foreach ($Cluster in $Clusters)
-{
-Write-Host "## #####" $Cluster "##### ##"
-$Cluster | Get-VMHost | Select-Object @{Label = "Host"; Expression = {$_.Name}},
-@{Label = "ESX Version"; Expression = {$_.version}},
-@{Label = "ESX Build" ; Expression = {$_.build}} | 
-Sort-Object -Property Host | 
-Format-Table -AutoSize -Wrap
+
+  foreach ($Cluster in $Clusters) {
+    Write-Host "## #####" $Cluster "##### ##"
+    $Cluster | Get-VMHost | 
+    Select-Object @{ Label = "Host"; Expression = { $_.Name }},
+    @{Label = "ESX Version"; Expression = { $_.version }},
+    @{Label = "ESX Build" ; Expression = { $_.build }} | 
+    Sort-Object -Property Host | 
+    Format-Table -AutoSize -Wrap
 }
